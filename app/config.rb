f8541c4304a74540afe6aefea1d42ec9141c0d22
yyunyun75 <yyunyun75@gmail.com>
2013-08-30 09:20:40 -0400

    move sass folder

--- app/config.rb
+++ app/config.rb
@@ -2,11 +2,11 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "app/css"
+css_dir = "css"
 sass_dir = "sass"
-images_dir = "app/img"
-javascripts_dir = "app/js"
-fonts_dir = "app/fonts"
+images_dir = "img"
+javascripts_dir = "js"
+fonts_dir = "fonts"
 
 output_style = :nested
 
